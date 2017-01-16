7debc18182283c05885e77bc4e1b5bc8486a1b0e
Jérémie Astori <jeremie@astori.fr>
2017-01-16 00:48:38 -0500

    Move the tokenizer to its own lib file to allow for testing

--- mocha-md-compiler.js
+++ mocha-md-compiler.js
@@ -2,43 +2,7 @@
 
 const fs = require('fs');
 
-const formats = {
-  command: /^<!--[\s\n]*fulky:([\s\S]+?)[\s\n]*-->/,
-  code: /^```(?:js|javascript)[\s\n]*\n+([\s\S]+?)```/,
-  newline: /^\s*\n+/,
-  unknown: /.*/
-};
-
-function tokenize(content) {
-  let match;
-  let token;
-
-  if (match = content.match(formats.newline)) {
-    token = null;
-  } else if (match = content.match(formats.code)) {
-    token = {
-      type: 'code',
-      code: match[1],
-    };
-
-  } else if (match = content.match(formats.command)) {
-    token = {
-      type: 'command',
-      command: match[1]
-    };
-  } else if (match = content.match(formats.unknown)) {
-    token = {
-      type: 'unknown'
-    };
-  } else {
-    throw 'boom'; // TODO Get rid of this
-  }
-
-  return {
-    contentRemainder: content.substring(match[0].length),
-    token: token
-  };
-}
+const tokenize = require('./lib/tokenize');
 
 function lex(content) {
   const tokens = [];
