e41129552651b0a0bb270ace95586ae2b87b0e80
Jérémie Astori <jeremie@astori.fr>
2017-01-30 00:50:01 -0500

    Add support and tests for usage with Jasmine
    Nothing needed changing in the code, but as I was very unfamiliar with Jasmine, it took me quite some time to figure this out.
    An almost identical test suite now ensures using fulky against Jasmine works correctly, but the default reporter (and only one that ships by default with Jasmine) is not good enough. I will improve this in the future.
    
    Also, add a tiny bit of documentation in the README file.
    
    BREAKING CHANGE: With Mocha, now use `--compilers=md:fulky/md-compiler`.

--- README.md
+++ README.md
@@ -3,3 +3,35 @@
 [![Build Status](https://travis-ci.org/astorije/fulky.svg?branch=master)](https://travis-ci.org/astorije/fulky)
 
 # fulky
+
+## Usage
+
+## Use it with [Mocha](https://mochajs.org/)
+
+Run it with:
+
+```sh
+mocha --compilers md:fulky/md-compiler *.md
+```
+
+## Use it with [Jasmine](https://jasmine.github.io/)
+
+In your `spec/support/jasmine.json` configuration file, add:
+
+```json
+{
+  "spec_dir": "spec",
+  "spec_files": [
+    "../*.md"
+  ],
+  "helpers": [
+    "../node_modules/fulky/md-compiler.js"
+  ]
+}
+```
+
+Then run it with:
+
+```sh
+jasmine
+```
--- test/runners/jasmine.test.js
+++ test/runners/jasmine.test.js
@@ -5,34 +5,31 @@ const spawnSync = require('child_process').spawnSync;
 
 function runFixture(fixture) {
   return spawnSync(
-    'node_modules/.bin/mocha',
-    ['--compilers', 'md:mocha-md-compiler', `test/fixtures/${fixture}.md`],
+    'node_modules/.bin/jasmine',
+    ['JASMINE_CONFIG_PATH=test/runners/jasmine.json', `--filter=${fixture}`],
     { encoding: 'utf-8' }
   );
 }
 
-describe('Executing Markdown fixtures against the Mocha compiler', function () {
-  // On dev and CI environments, each call to `spawnSync` takes about ~200-250ms
+describe('Executing Markdown fixtures against the Jasmine compiler', function () {
+  // On dev and CI environments, each call to `spawnSync` takes about ~150-200ms
   // so Mocha's default "slow" test threshold of 75ms always marks them in red.
-  // This marks tests > 300ms in yellow and > 600ms in red. It is suitable for
+  // This marks tests > 250ms in yellow and > 500ms in red. It is suitable for
   // tests with a single call to `spawnSync`.
-  this.slow(600);
+  this.slow(500);
 
   it('should pass a simple passing fixture', () => {
     const result = runFixture('basic-passing');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('Markdown');
-    expect(result.stdout).to.include(`${process.cwd()}/test/fixtures/basic-passing.md`);
-    expect(result.stdout).to.include("should run API example #1: require('assert').ok(true);");
-    expect(result.stdout).to.include('1 passing');
+    expect(result.stdout).to.include('1 spec');
   });
 
   it('should fail a simple failing fixture', () => {
     const result = runFixture('basic-failing');
 
     expect(result.status).to.equal(1);
-    expect(result.stdout).to.include('1 failing');
+    expect(result.stdout).to.include('1 failure');
     expect(result.stdout).to.include('AssertionError: false == true');
   });
 
@@ -40,8 +37,8 @@ describe('Executing Markdown fixtures against the Mocha compiler', function () {
     const result = runFixture('empty');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('0 passing');
-    expect(result.stdout).to.not.include('failing');
+    expect(result.stdout).to.include('0 specs');
+    expect(result.stdout).to.include('0 failures');
     expect(result.stderr).to.be.empty;
   });
 
@@ -49,35 +46,34 @@ describe('Executing Markdown fixtures against the Mocha compiler', function () {
     const result = runFixture('skip-test');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('1 passing');
-    expect(result.stdout).to.include("should run API example #1: require('assert').ok(true);");
+    expect(result.stdout).to.include('1 spec');
   });
 
   it('should save a `define`d example and prepend it later with `use`', () => {
     const result = runFixture('define-and-use');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('2 passing');
+    expect(result.stdout).to.include('2 specs');
   });
 
   it('should run body of `globals` once before all tests are run', () => {
     const result = runFixture('globals-comment');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('1 passing');
+    expect(result.stdout).to.include('1 spec');
   });
 
   it('should run a `globals` code block once before all tests are run', () => {
     const result = runFixture('globals-code-block');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('2 passing');
+    expect(result.stdout).to.include('2 specs');
   });
 
   it('should allow for multiple `globals` commands', () => {
     const result = runFixture('globals-multiple-times');
 
     expect(result.status).to.equal(0);
-    expect(result.stdout).to.include('1 passing');
+    expect(result.stdout).to.include('1 spec');
   });
 });
